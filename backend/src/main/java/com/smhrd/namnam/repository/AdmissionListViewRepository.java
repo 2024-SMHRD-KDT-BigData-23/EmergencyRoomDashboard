067577beae675cf8c0bdaf78a01c1a406dcefaac
이병헌 <155137287+LBH1226@users.noreply.github.com>
2024-05-28 21:42:44 +0900

    집 푸쉬
    특정 patient 이름, id에 대한 입원 내역 정보(검색, 가장최신) 로직 작성
    (두개 메서드 하나로 합치려 했으나 controller에서 하나의 메서드에서 두개의 값을 받아오게 되면 프론트에서 두개의 값 모두 입력해야 메서드가 돌아가는 문제)

--- backend/src/main/java/com/smhrd/namnam/repository/AdmissionListViewRepository.java
+++ backend/src/main/java/com/smhrd/namnam/repository/AdmissionListViewRepository.java
@@ -3,6 +3,7 @@ package com.smhrd.namnam.repository;
 import com.smhrd.namnam.entity.AdmissionListView;
 import com.smhrd.namnam.vo.AdmissionInfoVO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,7 +21,25 @@ public interface AdmissionListViewRepository extends JpaRepository<AdmissionList
     // 특정 입원코드에 대한 상세 정보
     List<AdmissionListView> findPatientDetailByAdmissionId(String admissionId);
 
-    // 특정 이름에 대한 입원 내역 정보(검색)
-    List<AdmissionListView> searchNameByPatientName(String patientName);
+    // 특정 이름에 대한 입원 내역 정보(검색, 가장최신)
+    @Query(value = "SELECT a.* FROM admission_list_view a " +
+            "JOIN (SELECT admission_id, MAX(patient_vital_created_at) AS max_vital_time " +
+            "FROM admission_list_view " +
+            "WHERE patient_name = :patientName " +
+            "GROUP BY admission_id) b " +
+            "ON a.admission_id = b.admission_id AND a.patient_vital_created_at = b.max_vital_time " +
+            "WHERE a.patient_name = :patientName",
+            nativeQuery = true)
+    List<AdmissionListView> searchByPatientName(String patientName);
 
+    // 특정 id에 대한 입원 내역 정보(검색, 가장최신)
+    @Query(value = "SELECT a.* FROM admission_list_view a " +
+            "JOIN (SELECT admission_id, MAX(patient_vital_created_at) AS max_vital_time " +
+            "FROM admission_list_view " +
+            "WHERE patient_id = :patientId " +
+            "GROUP BY admission_id) b " +
+            "ON a.admission_id = b.admission_id AND a.patient_vital_created_at = b.max_vital_time " +
+            "WHERE a.patient_id = :patientId",
+            nativeQuery = true)
+    List<AdmissionListView> searchByPatienId(String patientId);
 }
