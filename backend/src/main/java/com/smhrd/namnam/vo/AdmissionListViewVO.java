09818a61c2ea71479722d4e3e1d90e45838d44d1
이병헌 <155137287+LBH1226@users.noreply.github.com>
2024-05-28 09:35:01 +0900

    view 엔티티, 레파지토리 수정
    240528 09:34

--- backend/src/main/java/com/smhrd/namnam/vo/AdmissionListViewVO.java
+++ backend/src/main/java/com/smhrd/namnam/vo/AdmissionListViewVO.java
@@ -1,75 +1,58 @@
-package com.smhrd.namnam.entity;
+package com.smhrd.namnam.vo;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.hibernate.annotations.Immutable;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 @Data
-@Entity
-@Table(name = "admission_list_view")
-@Immutable
-public class AdmissionListView {
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdmissionListViewVO {
 
     // 입원 식별자
-    @Id
-    @Column(name = "admission_id")
     private Long admissionId;
 
     // 생체 데이터 등록 일자
-    @Column(name = "patient_vital_created_at")
     private Timestamp patientVitalCreatedAt;
 
     // 입원 상태
-    @Column(name = "admission_state")
     private String admissionState;
 
     // 환자 이름
-    @Column(name = "patient_name")
     private String patientName;
 
     // 환자 성별
-    @Column(name = "patient_sex")
     private String PatientSex;
 
     // 구역
-    @Column(name = "bed_ward")
     private String bedWard;
 
     // 체온
-    @Column(name = "patient_vital_temperature")
     private BigDecimal patientVitalTemperature;
 
     // 심박수
-    @Column(name = "patient_vital_hr")
     private int patientVitalHr;
 
     // 호흡수
-    @Column(name = "patient_vital_respiratory_rate")
     private int patientVitalRespiratoryRate;
 
     // 산소포화도
-    @Column(name = "patient_vital_spo2")
     private BigDecimal patientVitalspo2;
 
     // 수축 혈압
-    @Column(name = "patient_vital_nibp_s")
     private int patientVitalNibpS;
 
     // 이완 혈압
-    @Column(name = "patient_vital_nibp_d")
     private int patientVitalNibpD;
 
     // 위험도
-    @Column(name = "patient_vital_ncdss")
     private String patientVitalNcdss;
 
     // 실제 배치 결과
-    @Column(name = "admission_result_ward")
     private String admissionResultWard;
 }
