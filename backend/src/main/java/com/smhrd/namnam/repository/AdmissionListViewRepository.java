6ff2742a17d68a675dff9b7f76195f100a74d6ac
이병헌 <155137287+LBH1226@users.noreply.github.com>
2024-05-28 11:35:56 +0900

    Update AdmissionListViewRepository.java

--- backend/src/main/java/com/smhrd/namnam/repository/AdmissionListViewRepository.java
+++ backend/src/main/java/com/smhrd/namnam/repository/AdmissionListViewRepository.java
@@ -1,16 +1,13 @@
-//package com.smhrd.namnam.repository;
-//
-//import com.smhrd.namnam.entity.AdmissionListView;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Repository
-//public interface AdmissionListViewRepository extends JpaRepository<AdmissionListView, Long> {
-//
-//
-//
-//
-//}
+package com.smhrd.namnam.repository;
+
+import com.smhrd.namnam.entity.AdmissionListView;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AdmissionListViewRepository extends JpaRepository<AdmissionListView, Long> {
+
+}
